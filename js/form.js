5d5d299df9338b1244f2f6feb9a238b5d3ffede3
Piyush Malhotra <piyushmalhotra28@gmail.com>
2018-01-18 02:58:08 +0530

    Add contact form

---
+++ js/form.js
@@ -0,0 +1,57 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBl9zI9Hn8D70-GBX9nPU05DnmSuQpZKHk",
+authDomain: "starlight-0494.firebaseapp.com",
+databaseURL: "https://starlight-0494.firebaseio.com",
+projectId: "starlight-0494",
+storageBucket: "starlight-0494.appspot.com",
+messagingSenderId: "377384810896"
+};
+firebase.initializeApp(config);
+
+// Reference messages collections
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form Submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+    e.preventDefault();
+    var name = getInputVal('name');
+    var course = getInputVal('course');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(name, course, email, phone, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display='block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display='none';
+    }, 3000);
+
+    // Clear form
+    document.getElementById('contactForm').reset();
+}
+
+// to get form values
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+// Save messages to firebase
+function saveMessage(name, course, email, phone, message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        'name': name,
+        'course': course,
+        'email': email,
+        'phone': phone,
+        'message': message
+    })
+}
